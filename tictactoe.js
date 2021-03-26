91e400cfa24885bacfec0c8c8fd12d2d96090167
Gabriel Rezende <g@brielrezen.de>
2021-03-26 18:52:46 -0300

    multiplayer working

--- tictactoe.js
+++ tictactoe.js
@@ -15,13 +15,60 @@ cols.forEach(col => {
 })
 
 window.addEventListener('click', ev => {
+  // line 1:
+  // horizontal:
   if (cols[0].innerText != '_') {
     if (cols[0].innerText == cols[1].innerText && cols[0].innerText == cols[2].innerText) {
-      alert(`Vencedor: ${cols[0].innerText}`)
+      return winner()
     }
+    // vertical
     if (cols[0].innerText == cols[3].innerText && cols[0].innerText == cols[6].innerText) {
-      alert(`Vencedor: ${cols[0].innerText}`)
+      return winner()
     }
   }
-  
-}) 
+  // line 2:
+  if (cols[4].innerText != '_') {
+    // horizontal:
+    if (cols[3].innerText == cols[4].innerText && cols[3].innerText == cols[5].innerText) {
+      return winner()
+    }
+    // vertical
+    if (cols[4].innerText == cols[1].innerText && cols[4].innerText == cols[7].innerText) {
+      return winner()
+    }
+  }
+  // line 3:
+    if (cols[8].innerText != '_') {
+    // horizontal:
+    if (cols[6].innerText == cols[7].innerText && cols[6].innerText == cols[8].innerText) {
+      return winner()
+    }
+    // vertical:
+    if (cols[8].innerText == cols[2].innerText && cols[8].innerText == cols[5].innerText) {
+      return winner()
+    }
+  }
+  if (cols[4].innerText != '_') {
+      // diagonal 1
+      if (cols[0].innerText == cols[4].innerText && cols[0].innerText == cols[8].innerText){
+       return winner()
+      }
+      // diagonal 2
+      if (cols[2].innerText == cols[4].innerText && cols[2].innerText == cols[6].innerText){
+       return winner()
+      }
+  }
+})
+
+
+const winner = () => {
+  changePlayer()
+  alert(`Vencedor: ${player}`)
+  reset()
+}
+
+const reset = () => {
+  cols.forEach(col => {
+    col.innerHTML = '<p class="placeholder">_</p>'
+  })
+}
