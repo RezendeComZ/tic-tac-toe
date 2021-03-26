9e073f9e5d9b9b66e9f29b76a8c05092338de71d
Gabriel Rezende <g@brielrezen.de>
2021-03-26 20:00:20 -0300

    players names change

--- tictactoe.js
+++ tictactoe.js
@@ -1,14 +1,64 @@
-let cols = document.querySelectorAll('.col');
+// Todo:
+// Num keypad to play - transparent numbers on game
+// dark mode
+// console.table game at end
+// timer at console
 
-let player = 'X'
+const qSelector = div => {
+  return document.querySelectorAll(div)
+}
+
+let cols = qSelector('.col');
+
+const reset = () => {
+  cols.forEach(col => {
+    col.innerHTML = '<p class="placeholder">_</p>'
+  })
+}
+reset();
+
+let playerAName = 'Player A';
+let playerANameDiv = qSelector('#playerAName')[0];
+playerANameDiv.innerText = playerAName;
+let playerAPoints = '0'
+let playerAPointsDiv = qSelector('#playerAPoints')[0];
+playerAPointsDiv.innerText = playerAPoints;
+playerANameDiv.addEventListener('click', (ev) => {
+  ev.preventDefault();
+  playerAName = prompt('Player A name:')
+  if (playerAName != '') {
+    playerANameDiv.innerText = playerAName;
+  }
+})
+
+
+let playerBName = 'Player B';
+let playerBNameDiv = qSelector('#playerBName')[0];
+playerBNameDiv.innerText = playerBName;
+let playerBPoints = '0'
+let playerBPointsDiv = qSelector('#playerBPoints')[0];
+playerBPointsDiv.innerText = playerBPoints;
+playerBNameDiv.addEventListener('click', (ev) => {
+  ev.preventDefault();
+  playerBName = prompt('Player B name:')
+  if (playerBName != '') {
+    playerBNameDiv.innerText = playerBName;
+  }
+})
+
+let round = 1;
+let roundDiv = qSelector('#round')[0]
+roundDiv.innerText = round
+let playerTurn = 'X'
+
 const changePlayer = () => {
-  (player == 'X') ? player = 'O' : player = 'X'
+  (playerTurn == 'X') ? playerTurn = 'O' : playerTurn = 'X'
 }
 
 cols.forEach(col => {
   col.addEventListener('click', (ev) => {
     if (ev.target.classList[0] == 'placeholder') {
-      col.innerHTML = `<p class='played'>${player}</p>`
+      col.innerHTML = `<p class='played'>${playerTurn}</p>`
       changePlayer()
     }
   })
@@ -63,12 +113,19 @@ window.addEventListener('click', ev => {
 
 const winner = () => {
   changePlayer()
-  alert(`Winner: ${player}`)
+  let winnerName = '';
+  if (playerTurn == 'X') {
+    playerAPoints++;
+    playerAPointsDiv.innerText = playerAPoints;
+    winnerName = playerAName
+  } else {
+    playerBPoints++
+    playerBPointsDiv.innerText = playerBPoints;
+    winnerName = playerBName
+  }
+  alert(`Winner: ${winnerName}`)
+  round++;
+  roundDiv.innerText = round;
+  console.log(`Round ${round}. Winner: ${winnerName}. ${playerAName}: ${playerAPoints} - ${playerBName}: ${playerBPoints}` )
   reset()
-}
-
-const reset = () => {
-  cols.forEach(col => {
-    col.innerHTML = '<p class="placeholder">_</p>'
-  })
 }
